8af7d4e005a0236e680e8598e0eab8abd161c119
Kevin <kevinmaciel.daniel@gmail.com>
2021-03-19 18:27:17 -0300

    Primer version

---
+++ Controllers/CustomersController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customers
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Customers()
+        {
+            var customers = new List<Customer>
+            {//Borrar esta parte para probar cuando no hay elementos de la lista----------------
+                new Customer() {Name = "Kevin"},
+                new Customer() {Name = "Agustin"}
+            //----------------------------------------------------------------------------------
+            };
+            var viewModel = new Customer
+            {
+                Customers = customers
+            };
+
+            return View(viewModel);
+        }
+
+        public ActionResult Details(int id)
+        {
+            ViewBag.Name = id == 1 ? "Kevin Maciel" : "Nicolas Molina";
+
+
+
+            return View();
+        }
+
+
+
+    }
+}
---
+++ Models/Customer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Vidly.Models
+{
+    public class Customer
+    {
+        public string Name { get; set; }
+        public List<Customer> Customers { get; set; }
+    }
+
+    public class CustomersMovie
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
